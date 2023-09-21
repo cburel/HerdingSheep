fa1184f5673c85f407996ade2f1fbaee717ae28e
Celeste Burel <cburel@uccs.edu>
2023-09-21 14:08:32 -0600

    set up debug

--- DrivingSheep/MovingAgents/DrivingSheep.py
+++ DrivingSheep/MovingAgents/DrivingSheep.py
@@ -20,9 +20,68 @@ enemies = []
 for i in range (1, Constants.MAX_ENEMIES + 1):
 	enemies.append(Sheep(sheepImage, pygame.Vector2(random.randrange(0, bounds.x), random.randrange(0, bounds.y)), Constants.ENEMY_SIZE, Constants.ENEMY_SPD, Constants.ENEMY_COLOR, Constants.ENEMY_TURN_SPEED))
 
+
+def handleDebugging():        
+	#Handle the Debugging for Forces
+	events = pygame.event.get()
+	for event in events:
+		if event.type == pygame.KEYUP:
+
+			# Toggle Dog Influence
+			if event.key == pygame.K_1:
+				Constants.ENABLE_DOG = not Constants.ENABLE_DOG
+				print("Toggle Dog Influence", Constants.ENABLE_DOG)
+
+			# Toggle Alignment Influence
+			if event.key == pygame.K_2: 
+				Constants.ENABLE_ALIGNMENT = not Constants.ENABLE_ALIGNMENT
+				print("Toggle Alignment Influence", Constants.ENABLE_ALIGNMENT)
+
+			# Toggle Separation Influence
+			if event.key == pygame.K_3: 
+				Constants.ENABLE_SEPARATION = not Constants.ENABLE_SEPARATION
+				print("Toggle Separation Influence", Constants.ENABLE_SEPARATION)
+
+			# Toggle Cohesion Influence
+			if event.key == pygame.K_4: 
+				Constants.ENABLE_COHESION = not Constants.ENABLE_COHESION
+				print("Toggle Cohesion Influence", Constants.ENABLE_COHESION)
+
+			# Toggle Boundary Influence
+			if event.key == pygame.K_5: 
+				Constants.ENABLE_BOUNDARIES = not Constants.ENABLE_BOUNDARIES
+				print("Toggle Boundary Influence", Constants.ENABLE_BOUNDARIES)
+
+			# Toggle Dog Influence Lines
+			if event.key == pygame.K_6: 
+				Constants.DEBUG_DOG_INFLUENCE = not Constants.DEBUG_DOG_INFLUENCE
+				print("Toggle Dog Influence Lines", Constants.DEBUG_DOG_INFLUENCE)
+    
+			# Toggle Velocity Lines
+			if event.key == pygame.K_7: 
+				Constants.DEBUG_VELOCITY = not Constants.DEBUG_VELOCITY
+				print("Toggle Velocity Lines", Constants.DEBUG_VELOCITY)
+
+			# Toggle Neighbor Lines
+			if event.key == pygame.K_8: 
+				Constants.DEBUG_NEIGHBORS = not Constants.DEBUG_NEIGHBORS
+				print("Toggle Neighbor Lines", Constants.DEBUG_NEIGHBORS)
+
+			# Toggle Boundary Force Lines
+			if event.key == pygame.K_9: 
+				Constants.DEBUG_BOUNDARIES = not Constants.DEBUG_BOUNDARIES
+				print("Toggle Boundary Force Lines", Constants.DEBUG_BOUNDARIES)
+
+			# Toggle Bounding Box Lines
+			if event.key == pygame.K_0: 
+				Constants.DEBUG_BOUNDING_RECTS = not Constants.DEBUG_BOUNDING_RECTS
+				print("Toggle Bounding Box Lines", Constants.DEBUG_BOUNDING_RECTS)
+
 #main gameplay loop
 hasQuit = False
 while not hasQuit:
+
+	handleDebugging()
 
 	# event handler
 	for event in pygame.event.get():
